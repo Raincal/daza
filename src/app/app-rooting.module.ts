735ac33fcd33b2c4a5c4461dddbab6b89e5bb496
Raincal <cyj94228@gmail.com>
2016-10-17 17:05:37 +0800

    fix md-list-avatar

--- src/app/app-rooting.module.ts
+++ src/app/app-rooting.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-
 import { AccountRoutes } from './account';
 import { HomeRoutes } from './home';
 
 const routes: Routes = [
-
-  {
-    path: '', component: HomeComponent, children: [
-      ...HomeRoutes,
-    ]
-  },
+  ...HomeRoutes,
   ...AccountRoutes,
   { path: '**', redirectTo: '/home/latest', pathMatch: 'full' }
 ];
--- src/app/home/home.routing.ts
+++ src/app/home/home.routing.ts
@@ -3,11 +3,16 @@ import { Routes } from '@angular/router';
 import { ArticleComponent } from './article/article.component';
 import { TagComponent } from './tag/tag.component';
 import { TopicComponent } from './topic/topic.component';
+import { HomeComponent } from './home.component';
 
 export const HomeRoutes: Routes = [
-  { path: '', redirectTo: '/home/latest', pathMatch: 'full' },
-  { path: 'home/:slug', component: ArticleComponent },
-  { path: 'home/latest', component: ArticleComponent },
-  { path: '', outlet: 'tag', component: TagComponent },
-  { path: '', outlet: 'topic', component: TopicComponent }
+  {
+    path: '', component: HomeComponent, children: [
+      { path: '', redirectTo: '/home/latest', pathMatch: 'full' },
+      { path: 'home/:slug', component: ArticleComponent },
+      { path: 'home/latest', component: ArticleComponent },
+      { path: '', outlet: 'tag', component: TagComponent },
+      { path: '', outlet: 'topic', component: TopicComponent }
+    ]
+  },
 ];
