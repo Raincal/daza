9e163d6074e8d75773bbef0fec2aae5352f9a85a
Raincal <cyj94228@gmail.com>
2016-12-23 12:41:13 +0800

    update Angular2 version to 2.4.1

--- dist/styles.5f7f16a82a4f91544984.bundle.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([1,3],{1111:function(n,o,c){c(504),c(503),n.exports=c(502)},502:function(n,o){},503:function(n,o){},504:function(n,o){}},[1111]);
-//# sourceMappingURL=styles.5f7f16a82a4f91544984.bundle.map
