8446e2cf24a9568f4089ae3776f84b0b543e09be
Raincal <cyj94228@gmail.com>
2017-02-12 00:07:57 +0800

    optimizing bundle size

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,18 @@ import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
-import { MaterialModule } from '@angular/material';
+
+import { MdButtonModule } from '@angular/material/button';
+import { MdToolbarModule } from '@angular/material/toolbar';
+import { MdCardModule } from '@angular/material/card';
+import { MdListModule } from '@angular/material/list';
+import { MdTabsModule } from '@angular/material/tabs';
+import { MdMenuModule } from '@angular/material/menu';
+import { MdIconModule } from '@angular/material/icon';
+import { MdInputModule } from '@angular/material/input';
+import { MdGridListModule } from '@angular/material/grid-list';
+import { MdProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 import { Ng2PaginationModule } from 'ng2-pagination';
 import { ToastModule } from 'ng2-toastr/ng2-toastr';
 
@@ -30,7 +41,16 @@ const options: any = {
     RouterModule,
     Ng2PaginationModule,
     ToastModule.forRoot(options),
-    MaterialModule.forRoot()
+    MdButtonModule.forRoot(),
+    MdToolbarModule.forRoot(),
+    MdCardModule.forRoot(),
+    MdListModule.forRoot(),
+    MdTabsModule.forRoot(),
+    MdMenuModule.forRoot(),
+    MdIconModule.forRoot(),
+    MdInputModule.forRoot(),
+    MdGridListModule.forRoot(),
+    MdProgressSpinnerModule.forRoot()
   ],
   providers: [
     SpinnerService
@@ -41,7 +61,16 @@ const options: any = {
     ReactiveFormsModule,
     HttpModule,
     RouterModule,
-    MaterialModule,
+    MdButtonModule,
+    MdToolbarModule,
+    MdCardModule,
+    MdListModule,
+    MdTabsModule,
+    MdMenuModule,
+    MdIconModule,
+    MdInputModule,
+    MdGridListModule,
+    MdProgressSpinnerModule,
     Ng2PaginationModule,
     ToastModule,
     SpinnerComponent,
