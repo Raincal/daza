a89c53a7fe89be9b7ae583aca17be86843bdd95d
Raincal <cyj94228@gmail.com>
2017-05-25 20:00:23 +0800

    update Angular version to 4.1.3

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,16 +4,18 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
-import { MdButtonModule } from '@angular/material/button';
-import { MdToolbarModule } from '@angular/material/toolbar';
-import { MdCardModule } from '@angular/material/card';
-import { MdListModule } from '@angular/material/list';
-import { MdTabsModule } from '@angular/material/tabs';
-import { MdMenuModule } from '@angular/material/menu';
-import { MdIconModule } from '@angular/material/icon';
-import { MdInputModule } from '@angular/material/input';
-import { MdGridListModule } from '@angular/material/grid-list';
-import { MdProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {
+  MdButtonModule,
+  MdToolbarModule,
+  MdCardModule,
+  MdListModule,
+  MdTabsModule,
+  MdMenuModule,
+  MdIconModule,
+  MdInputModule,
+  MdGridListModule,
+  MdProgressSpinnerModule
+} from '@angular/material';
 
 import { Ng2PaginationModule } from 'ng2-pagination';
 import { ToastyModule } from 'ng2-toasty';
